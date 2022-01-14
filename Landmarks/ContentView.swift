06daa04211e002654402885560f0af8d8d4f3af0
Aleksei Izmalkin <aleksei_izmalkin@epam.com>
2022-01-14 20:49:57 +0400

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Aleksei Izmalkin on 14.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
