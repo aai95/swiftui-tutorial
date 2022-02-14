9d32bc3db694de9b117f929a55ff3cb23bfaf45b
Aleksei Izmalkin <aleksei_izmalkin@epam.com>
2022-02-14 20:45:07 +0400

    Add a watchOS Target

---
+++ WatchLandmarks WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Aleksei Izmalkin on 14.02.2022.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
